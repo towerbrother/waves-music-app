40a49e0be34a8f510988ca56828d8bec0cb01b02
GTO <giorgio.torre@stellenanzeigen.de>
2022-02-08 08:43:00 +0100

    added initial styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import "./styles/app.scss";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Waves Music App</h1>
       <Song />
       <Player />
     </div>
