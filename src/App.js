a791fd2d5a9bb94e030e6d516705ba2836bdbe45
GTO <giorgio.torre@stellenanzeigen.de>
2022-02-08 09:07:05 +0100

    added song state and styling to Song component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./util";
 import "./styles/app.scss";
 
 const App = () => {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
